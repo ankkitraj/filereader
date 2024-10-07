4fbb0259e457d0a88dd3be53c469b558f040bc5e
ankkit <ankkitraj4@hotmail.comgit config --global user.email ankkitraj4@hotmail.com>
2024-10-07 18:29:52 +0200

    dockerfile and removed simple app

--- simplenodeapp/routes/fileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const fileController = require('../controllers/fileController');
-
-// Define the route to read the file
-router.get('/read', fileController.readFile);
-
-module.exports = router;
